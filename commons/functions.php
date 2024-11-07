e1b093faaf811942b0f0ebe8f0f40444c3d8e0e5
antrc2 <antrc2gamer@gmail.com>
2024-11-07 13:27:22 +0700

    add CRUD ctgr and prd admin

--- commons/functions.php
+++ commons/functions.php
@@ -1,5 +1,5 @@
 <?php
     function database(){
-        return new PDO("mysql:host=localhost;dbname=project1","root","");
+        return new PDO("mysql:host=localhost;dbname=project1","root","Sqrtfl0@t01");
     }
 ?>
