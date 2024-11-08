76bb9c75e8bee4bc6b0df5ce98375792f7e6efe8
antrc2 <antrc2gamer@gmail.com>
2024-11-08 14:53:11 +0700

    add header after 1s with sweetalert2

--- commons/functions.php
+++ commons/functions.php
@@ -10,4 +10,12 @@
   icon: '$icon'
 })</script>";
     }
+    function headerAfterXSecondWithSweetAlert2($location, $time, $icon,$message){
+        echo "<script>
+        setTimeout(function() {
+                    window.location.href = '${location}';
+                }, ${time});
+                </script>";
+        echo SweetAlert2($icon, $message);
+    }
 ?>
